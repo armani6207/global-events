c8b28a266f77b430d243018f9ea3bd4fa57ced44
Armando <rojas.armani@gmail.com>
2020-12-06 15:32:23 -0500

    initial commit

---
+++ lib/global_events.rb
@@ -0,0 +1,6 @@
+require "global_events/version"
+
+module GlobalEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/global_events/version.rb
@@ -0,0 +1,3 @@
+module GlobalEvents
+  VERSION = "0.1.0"
+end
